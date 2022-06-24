e35bfb326d7cd0c165bff8a66335721761f6d0ff
Ammardapi <muammargeria7@gmail.com>
2022-06-24 10:46:33 +0700

    imageview

--- app/src/main/java/com/dapi/lomboktravelling/DashboardActivity.java
+++ app/src/main/java/com/dapi/lomboktravelling/DashboardActivity.java
@@ -3,6 +3,7 @@ package com.dapi.lomboktravelling;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class DashboardActivity extends AppCompatActivity {
 
@@ -10,5 +11,18 @@ public class DashboardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        ImageView pantai =findViewById(R.id.iv_beach);
+        pantai.setImageResource(R.drawable.pantai);
+        ImageView hill =findViewById(R.id.iv_hill);
+        hill.setImageResource(R.drawable.hill);
+        ImageView terjun =findViewById(R.id.iv_waterfall);
+        terjun.setImageResource(R.drawable.terjun);
+        ImageView rest =findViewById(R.id.iv_rest);
+        rest.setImageResource(R.drawable.rest);
+        ImageView culture =findViewById(R.id.iv_culture);
+        culture.setImageResource(R.drawable.budaya);
+        ImageView religi =findViewById(R.id.iv_religi);
+        religi.setImageResource(R.drawable.religi);
     }
 }
--- app/src/main/java/com/dapi/lomboktravelling/MainActivity.java
+++ app/src/main/java/com/dapi/lomboktravelling/MainActivity.java
@@ -9,6 +9,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -35,6 +36,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
 
         // Initialize Firebase Auth
         mAuth = FirebaseAuth.getInstance();
